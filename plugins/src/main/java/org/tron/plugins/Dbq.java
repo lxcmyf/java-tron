5c4eca24987a6e266119ffae522e0b3f117cd0a1
liuxincheng <lxcmyf@qq.com>
2025-06-11 12:53:31 +0800

    feat(tmp): account random

--- plugins/src/main/java/org/tron/plugins/Dbq.java
+++ plugins/src/main/java/org/tron/plugins/Dbq.java
@@ -1,5 +1,6 @@
 package org.tron.plugins;
 
+import com.google.protobuf.ByteString;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.Callable;
 import lombok.extern.slf4j.Slf4j;
 import org.rocksdb.RocksDBException;
@@ -14,6 +16,7 @@ import org.tron.plugins.utils.ByteArray;
 import org.tron.plugins.utils.db.DBInterface;
 import org.tron.plugins.utils.db.DBIterator;
 import org.tron.plugins.utils.db.DbTool;
+import org.tron.protos.Protocol;
 import picocli.CommandLine;
 
 @Slf4j(topic = "qd")
@@ -24,6 +27,10 @@ import picocli.CommandLine;
         "0:Successful",
         "n:query failed,please check toolkit.log"})
 public class Dbq implements Callable<Integer> {
+  private static final String OWNER_ADDRESS;
+  static {
+    OWNER_ADDRESS = "41548794500882809695a8a687866e76d4271a1abc";
+  }
 
   @CommandLine.Spec
   CommandLine.Model.CommandSpec spec;
@@ -78,21 +85,61 @@ public class Dbq implements Callable<Integer> {
 //    return 0;
 //  }
 
-  private int query() throws RocksDBException, IOException {
-    try (DBInterface database = DbTool.getDB(this.db.getParent(), this.db.getFileName().toString())) {
-      DBIterator iterator = database.iterator();
-      long start = System.nanoTime();
-      int i = 0;
-      for (iterator.seekToFirst(); iterator.hasNext(); iterator.next()) {
-        if (i >= 1000000) {
-          break;
-        }
-        iterator.getValue();
-        i++;
-      }
-      long end = System.nanoTime();
-      spec.commandLine().getOut().format("耗时: %d μs", (end - start) / 1000).println();
+//  private int query() throws RocksDBException, IOException {
+//    try (DBInterface database = DbTool.getDB(this.db.getParent(), this.db.getFileName().toString())) {
+//      DBIterator iterator = database.iterator();
+//      long start = System.nanoTime();
+//      int i = 0;
+//      for (iterator.seekToFirst(); iterator.hasNext(); iterator.next()) {
+//        if (i >= 1000000) {
+//          break;
+//        }
+//        iterator.getValue();
+//        i++;
+//      }
+//      long end = System.nanoTime();
+//      spec.commandLine().getOut().format("耗时: %d μs", (end - start) / 1000).println();
+//    }
+//    return 0;
+//  }
+
+  private int query() {
+    int count = 1_000_000;  // 消息数量
+    List<Protocol.Account> accounts = new ArrayList<>(count);
+
+    // 构造测试数据
+    for (int i = 0; i < count; i++) {
+      ByteString addressByte = ByteString.copyFrom(ByteArray.fromHexString(OWNER_ADDRESS));
+      ByteString name = ByteString.copyFrom(("name" + i).getBytes());
+      ByteString id = ByteString.copyFrom(UUID.randomUUID().toString().getBytes());
+      Protocol.Account account = Protocol.Account.newBuilder()
+          .setAddress(addressByte)
+          .setAccountName(name).setAccountId(id)
+          .build();
+      accounts.add(account);
     }
+
+    // 测试序列化
+    long startSerialization = System.nanoTime();
+    List<byte[]> serializedData = new ArrayList<>(count);
+    for (Protocol.Account account : accounts) {
+      serializedData.add(account.toByteArray());
+    }
+    long endSerialization = System.nanoTime();
+    spec.commandLine().getOut().format("耗时: %d μs", (endSerialization - startSerialization) / 1000).println();
+
+    // 测试反序列化
+    long startDeserialization = System.nanoTime();
+    List<Protocol.Account> deserializedPeople = new ArrayList<>(count);
+    for (byte[] data : serializedData) {
+      try {
+        deserializedPeople.add(Protocol.Account.parseFrom(data));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    long endDeserialization = System.nanoTime();
+    spec.commandLine().getOut().format("耗时: %d μs", (endDeserialization - startDeserialization) / 1000).println();
     return 0;
   }
 
